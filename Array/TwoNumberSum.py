45210c31455fe19932e79296d20a9f6c8b53cb29
Mira M-Gh <55577054+miramgh@users.noreply.github.com>
2020-09-14 17:33:16 +0200

    Delete TwoNumberSum.py

--- Array/TwoNumberSum.py
+++
@@ -1,9 +0,0 @@
-def twoNumberSum(array, targetSum):
-    
-	for i in range (len(array)-1):
-		firstNum = array[i]
-		for j in range (i+1,len(array)):
-			secondNum =array[j]
-			if firstNum +secondNum ==targetSum:
-				return [firstNum,secondNum]
-	return []
